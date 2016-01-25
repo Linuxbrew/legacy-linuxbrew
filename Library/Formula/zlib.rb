800b52339bd45a6584c07a8058d645706b68bc93
Shaun Jackman <sjackman@gmail.com>
2016-01-24 22:54:10 -0800

    zlib: Update from homebrew/dupes for Linuxbrew
    Closes #746.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Library/Formula/zlib.rb
+++ Library/Formula/zlib.rb
@@ -1,17 +1,16 @@
-require 'formula'
-
 class Zlib < Formula
-  homepage 'http://www.zlib.net/'
-  url 'http://zlib.net/zlib-1.2.8.tar.gz'
-  mirror 'https://downloads.sourceforge.net/project/libpng/zlib/1.2.8/zlib-1.2.8.tar.gz'
-  sha1 'a4d316c404ff54ca545ea71a27af7dbc29817088'
+  desc "General-purpose lossless data-compression library"
+  homepage "http://www.zlib.net/"
+  url "http://zlib.net/zlib-1.2.8.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/libpng/zlib/1.2.8/zlib-1.2.8.tar.gz"
+  sha256 "36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d"
 
   bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/dupes"
     cellar :any
-    sha1 "8111b1453b1ac71c0249f2e3abe2ce16372509af" => :yosemite
-    sha1 "4902071981b276d201681dc4321534f94d3f32c5" => :mavericks
-    sha1 "e431acef3aa6c11d9ae8a68c29e37d3bd7400492" => :mountain_lion
+    revision 1
+    sha256 "2971abbd45572722af5043a74ecf8a5bfd06adc9834ec90e4126a34c6ce982a1" => :yosemite
+    sha256 "adc394a9e296003bc2bc88451c649aec019496cb6ed3d6673005fcd818ae44a5" => :mavericks
+    sha256 "6ece1cb4b656f0f7ef1feab95ef6eb183e2f9ee2448c1e034de1a97d7f9da249" => :mountain_lion
   end
 
   keg_only :provided_by_osx
@@ -23,10 +22,27 @@ class Zlib < Formula
   # See: https://github.com/Homebrew/homebrew-dupes/pull/228
   patch :DATA
 
+  # http://zlib.net/zlib_how.html
+  resource "test_artifact" do
+    url "http://zlib.net/zpipe.c"
+    version "20051211"
+    sha256 "68140a82582ede938159630bca0fb13a93b4bf1cb2e85b08943c26242cf8f3a6"
+  end
+
   def install
     ENV.universal_binary if build.universal?
     system "./configure", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    testpath.install resource("test_artifact")
+    system ENV.cc, "zpipe.c", "-I#{include}", "-L#{lib}", "-lz", "-o", "zpipe"
+
+    touch "foo.txt"
+    output = ("./zpipe < foo.txt > foo.txt.z")
+    system output
+    assert File.exist?("foo.txt.z")
   end
 end
 

0a6f08b92575f4bda4deea72861d7526a4b868c0
Shaun Jackman <sjackman@gmail.com>
2016-02-01 15:27:53 -0800

    util-linux 2.27.1 (new formula) for Linuxbrew
    Closes #777.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

---
+++ Library/Formula/util-linux.rb
@@ -0,0 +1,23 @@
+class UtilLinux < Formula
+  desc "Collection of Linux utilities"
+  homepage "https://github.com/karelzak/util-linux"
+  url "https://www.kernel.org/pub/linux/utils/util-linux/v2.27/util-linux-2.27.1.tar.xz"
+  sha256 "0a818fcdede99aec43ffe6ca5b5388bff80d162f2f7bd4541dca94fecb87a290"
+  head "https://github.com/karelzak/util-linux.git"
+  # tag "linuxbrew"
+
+  def install
+    system "./configure",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--prefix=#{prefix}",
+      # Fix chgrp: changing group of 'wall': Operation not permitted
+      "--disable-use-tty-group"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "January", shell_output("#{bin}/cal 1 2016")
+  end
+end

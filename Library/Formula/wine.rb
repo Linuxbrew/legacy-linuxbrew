6a8d3d772c6085e9fedfddd72c357471fa6cd500
Shaun Jackman <sjackman@gmail.com>
2016-02-16 23:08:33 -0800

    wine: Disable ENV.m32 for Linuxbrew
    Closes Linuxbrew/linuxbrew#821

--- Library/Formula/wine.rb
+++ Library/Formula/wine.rb
@@ -115,7 +115,7 @@ class Wine < Formula
   end
 
   def install
-    ENV.m32 # Build 32-bit; Wine doesn't support 64-bit host builds on OS X.
+    ENV.m32 if OS.mac? # Build 32-bit; Wine doesn't support 64-bit host builds on OS X.
 
     # Help configure find libxml2 in an XCode only (no CLT) installation.
     ENV.libxml2

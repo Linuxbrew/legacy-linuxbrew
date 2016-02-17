c9aaf78c94b4fdfe3f096fa50af8c8a4c33a1a0d
Shaun Jackman <sjackman@gmail.com>
2016-02-17 09:55:11 -0800

    gpatch: update 2.7.5 bottle for Linuxbrew.

--- Library/Formula/gpatch.rb
+++ Library/Formula/gpatch.rb
@@ -11,6 +11,7 @@ class Gpatch < Formula
     sha256 "526112f0034e80e4b830d6a4b486073be6ea79743a1f4cb7b96e7564bf9dbae4" => :el_capitan
     sha256 "6de31643dafee3863e42bfff45ad8b128bd45fa02ea46a9a98732ad84e8b59ed" => :yosemite
     sha256 "0eee795f5f3c9d9e33bfcf83a207cea34fa41f4077fe99e0edc3ff894aedad41" => :mavericks
+    sha256 "425e53ed3deb72838fbe14c5ce8e079917db629dd502d36c50f0ea028635bcb1" => :x86_64_linux
   end
 
   def install

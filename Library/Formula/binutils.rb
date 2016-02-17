4c1236c379dbbd814de04c9d37d4f2bb645d97cc
Shaun Jackman <sjackman@gmail.com>
2016-02-16 22:17:27 -0800

    binutils: update 2.25.1 bottle for Linuxbrew.
    Built on CentOS 5.11 with glibc 2.5.

--- Library/Formula/binutils.rb
+++ Library/Formula/binutils.rb
@@ -12,10 +12,12 @@ class Binutils < Formula
   depends_on "zlib" => :recommended unless OS.mac?
 
   bottle do
+    cellar :any if OS.linux?
     sha256 "ab393fd01e8bb2f9ff697efa3e14765eb32468a46380ce8c2db0d5baf1b588b5" => :el_capitan
     sha256 "299f9bbdf522b68b803ce6cdb109a34b018459f3815771b3530ba95e06ee19f7" => :yosemite
     sha256 "30b13f1b4ddfe09f5140c2f00d0d34d42ed471ed2ab241405d8f5b7d3fb8eb41" => :mavericks
     sha256 "689d5c651fba64a18fed5f8f66ba9d156af7f3b8787e383afe8979f535432d64" => :mountain_lion
+    sha256 "db25a6b21bc82820ab5e4f14d0688ee826c03a224eb0e7ae9247ec773b86b810" => :x86_64_linux
   end
 
   # Fixes build on 10.6. Committed upstream; will be in the next release:

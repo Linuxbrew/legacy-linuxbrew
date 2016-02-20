cb4589a371573356a0a310af343314efaccd6469
Shaun Jackman <sjackman@gmail.com>
2016-02-19 21:31:21 -0800

    glibc: update 2.19 bottle for Linuxbrew.

--- Library/Formula/glibc.rb
+++ Library/Formula/glibc.rb
@@ -5,6 +5,12 @@ class Glibc < Formula
   sha256 "2e293f714187044633264cd9ce0183c70c3aa960a2f77812a6390a3822694d15"
   # tag "linuxbrew"
 
+  bottle do
+    prefix "/home/linuxbrew/.linuxbrew"
+    cellar "/home/linuxbrew/.linuxbrew/Cellar"
+    sha256 "80eef9cbc1f5b2907bc137ae1448175707dba1eed6a5d8764d9ddcfd74c6b9da" => :x86_64_linux
+  end
+
   option "with-current-kernel", "Compile for compatibility with kernel not older than your current one"
 
   # binutils 2.20 or later is required

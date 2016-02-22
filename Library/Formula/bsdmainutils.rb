4cd7fe36cc3c75b1e9eac7077e33f17647fd622d
Shaun Jackman <sjackman@gmail.com>
2016-02-22 12:28:02 -0800

    bsdmainutils: Add desc and tag for Linuxbrew

--- Library/Formula/bsdmainutils.rb
+++ Library/Formula/bsdmainutils.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class Bsdmainutils < Formula
+  desc "Collection of utilities from FreeBSD"
   homepage "https://packages.debian.org/sid/bsdmainutils"
   url "http://ftp.debian.org/debian/pool/main/b/bsdmainutils/bsdmainutils_9.0.6.tar.gz"
-  sha1 "26a24856e8e026eeca8978a8b5074c83c7fd0db2"
+  sha256 "48868ac99c8dd92a69bb430e6bdf865602522ad3a2f5a0dd9cae77b46fc93b57"
+  # tag "linuxbrew"
 
   def install
     system "for i in `<debian/patches/series`; do patch -p1 <debian/patches/$i; done"

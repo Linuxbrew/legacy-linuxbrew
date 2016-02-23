451644b3490e19cb0744149c7daa9cca4ed289d8
Shaun Jackman <sjackman@gmail.com>
2016-02-23 09:50:56 -0800

    binutils: update 2.25.1 bottle for Linuxbrew.
    The previous bottle had RPATH unintentionally set to
    /home/sjackman/.linuxbrew/lib

--- Library/Formula/binutils.rb
+++ Library/Formula/binutils.rb
@@ -17,7 +17,7 @@ class Binutils < Formula
     sha256 "299f9bbdf522b68b803ce6cdb109a34b018459f3815771b3530ba95e06ee19f7" => :yosemite
     sha256 "30b13f1b4ddfe09f5140c2f00d0d34d42ed471ed2ab241405d8f5b7d3fb8eb41" => :mavericks
     sha256 "689d5c651fba64a18fed5f8f66ba9d156af7f3b8787e383afe8979f535432d64" => :mountain_lion
-    sha256 "db25a6b21bc82820ab5e4f14d0688ee826c03a224eb0e7ae9247ec773b86b810" => :x86_64_linux
+    sha256 "6ad6a329100725d635b6cb9ae14a0c469398c735f2baccaa6422763c806ecfdc" => :x86_64_linux
   end
 
   # Fixes build on 10.6. Committed upstream; will be in the next release:

65c263d74c70a20989a1b9afcd28006793c6a262
David Holm <david.holm@ericsson.com>
2016-03-02 13:37:16 +0100

    libfuse 2.9.5 (new formula) for Linuxbrew
    Closes Linuxbrew/linuxbrew#873.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

---
+++ Library/Formula/libfuse.rb
@@ -0,0 +1,33 @@
+class Libfuse < Formula
+  desc "Reference implementation of the Linux FUSE interface."
+  homepage "https://github.com/libfuse/libfuse"
+  url "https://github.com/libfuse/libfuse/archive/fuse_2_9_5.tar.gz"
+  sha256 "ccea9c00f7572385e9064bc55b2bfefd8d34de487ba16d9eb09672202b5440ec"
+  head "https://github.com/libfuse/libfuse.git"
+  # tag "linuxbrew"
+
+  depends_on "gettext"
+
+  def install
+    cp Formula["gettext"].pkgshare/"config.rpath", "."
+    system "./makeconf.sh"
+    ENV["MOUNT_FUSE_PATH"] = "#{sbin}/"
+    ENV["UDEV_RULES_PATH"] = "#{etc}/udev/rules.d"
+    ENV["INIT_D_PATH"] = "#{etc}/init.d"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"fuse-test.c").write <<-EOS.undent
+      #include <fuse.h>
+      #include <stdlib.h>
+      int main(int argc, char** argv)
+      {
+        struct fuse_operations ops = {NULL, };
+        return fuse_main(argc, argv, &ops, NULL);
+      }
+    EOS
+    system "$CC $CPPFLAGS -DFUSE_USE_VERSION=26 -D_FILE_OFFSET_BITS=64 fuse-test.c $LDFLAGS -lfuse"
+  end
+end

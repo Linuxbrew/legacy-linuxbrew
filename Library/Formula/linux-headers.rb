4b940e5cc0f3c6a26d15a9ce18d2f0ba9416e2f0
Shaun Jackman <sjackman@gmail.com>
2016-03-04 17:32:56 -0800

    linux-headers: Use .gz rather than .xz
    Fix the error:
    ==> Verifying git-2.7.2.tar.xz checksum
    Error: Empty archive

--- Library/Formula/linux-headers.rb
+++ Library/Formula/linux-headers.rb
@@ -1,8 +1,8 @@
 class LinuxHeaders < Formula
   desc "Header files of the Linux kernel"
   homepage "http://kernel.org/"
-  url "https://cdn.kernel.org/pub/linux/kernel/v3.x/linux-3.18.27.tar.xz"
-  sha256 "ac9d2cd940e01cb2a659831fde4976e60d0241b0374026ed2ed7036d07039f06"
+  url "https://cdn.kernel.org/pub/linux/kernel/v3.x/linux-3.18.27.tar.gz"
+  sha256 "35618751139c2ad76298dd5e2e4b80121c1a14f490375bc93a35b9f0b882d29c"
   # tag "linuxbrew"
 
   bottle do

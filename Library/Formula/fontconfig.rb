f3bef9aac396195ce58af3f07b264098c700287c
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2016-03-20 16:02:39 +0100

    fontconfig: Depends on expat for Linuxbrew
    Closes Linuxbrew/linuxbrew#960.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Library/Formula/fontconfig.rb
+++ Library/Formula/fontconfig.rb
@@ -27,6 +27,7 @@ class Fontconfig < Formula
 
   depends_on "pkg-config" => :build
   depends_on "freetype"
+  depends_on "expat" unless OS.mac?
 
   # Reverts commit http://cgit.freedesktop.org/fontconfig/commit/?id=7a6622f25cdfab5ab775324bef1833b67109801b,
   # which breaks caching font directories containing subdirectories

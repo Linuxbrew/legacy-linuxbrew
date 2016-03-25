580f370394a79292bee2b77d3ca5b0584b9e9c51
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-03-25 07:01:26 +0000

    pcre: update 8.38 bottle for Linuxbrew.

--- Library/Formula/pcre.rb
+++ Library/Formula/pcre.rb
@@ -6,10 +6,11 @@ class Pcre < Formula
   sha256 "b9e02d36e23024d6c02a2e5b25204b3a4fa6ade43e0a5f869f254f49535079df"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
     sha256 "ef6908428e587406f50eb4d09813c823f8a6748324425930681bfcee271211df" => :el_capitan
     sha256 "96b7b6dc6b5e5efb1d736f3a035ca2c01f045f3ba80b80744548e5de6b1c5fe1" => :yosemite
     sha256 "fb5e4de4665dd83c6516a14d1d8ad41e80a1de608212e2aac14fd4e04b1d4341" => :mavericks
+    sha256 "38d15959fa7d8e14b6b3d030d7c1c8d32e56ea12324bba99496d675a7056368a" => :x86_64_linux
   end
 
   head do

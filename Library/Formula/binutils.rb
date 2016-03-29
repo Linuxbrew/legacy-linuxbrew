b05f09fdffc0c096eb1f0a128314ba158a6f88a7
Shaun Jackman <sjackman@gmail.com>
2016-03-29 10:51:44 -0700

    binutils: update 2.26 bottle for Linuxbrew.
    Built using gcc 4.4.7 and glibc 2.5 on CentOS 5.11.

--- Library/Formula/binutils.rb
+++ Library/Formula/binutils.rb
@@ -10,6 +10,7 @@ class Binutils < Formula
     sha256 "6290d3d2b56f5b9450043d1f71d0597e7405e6d27c1c6eddd54f1f6bb5ff96f0" => :el_capitan
     sha256 "12f87ef4be8a0ea5d92cf9ef2f6bb3db8b1836c61d7ab0a95ddcea3a6b8dc37f" => :yosemite
     sha256 "a82e26a42ba2775534c7b3651feacfd1d3be7b4e83cfbe3635c7f728ac2cbb4d" => :mavericks
+    sha256 "ecf2582b99adc8f66358211c434edee99e81ead1c5c5baa55e09c4d8c59a8d1f" => :x86_64_linux
   end
 
   # No --default-names option as it interferes with Homebrew builds.
